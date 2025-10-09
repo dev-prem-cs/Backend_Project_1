f5fcd0d80b1de6865aa7f962467d3baf6ee11ade
dev-prem-cs <premcs.exe@gmail.com>
2025-10-09 21:39:10 +0530

    fix: update user route for account details and add missing getUserChannelProfile import

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router } from "express";
-import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 export const userRouter=Router();
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -23,6 +23,11 @@ userRouter.post("/logout",verifyJWT,logoutUser)
 userRouter.post("/refresh-token",refreshAccessToken)
 userRouter.post("/changePassword",verifyJWT,changeCurrentPassword)
 userRouter.get("/info",verifyJWT,getCurrentUser)
-userRouter.post("/update",verifyJWT,updateAccountDetails)
+userRouter.patch("/update",verifyJWT,updateAccountDetails)
 userRouter.post("/update-avatar",verifyJWT,upload.single("avatar"),updateUserAvatar)
 userRouter.post("/update-coverImage",verifyJWT,upload.single("coverImage"),updateUserCoverImage)     
+
+// pending testing  
+
+userRouter.get("/ch/:username",verifyJWT,getUserChannelProfile)
+userRouter.get("/history",verifyJWT,getUserChannelProfile)
